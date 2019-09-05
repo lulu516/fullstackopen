cba29ddd38cf66f7a5126aa4be2eec2bcd080144
lulu516 <lulu19910516@gmail.com>
2019-09-05 11:57:06 +0200

    finish 1a introduction to react

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by
+      <a href="https://github.com/lulu516/fullstackopen">Lulu</a>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Hello = props => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = "Ruud";
+  const age = 34;
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Lulu" age={12 + 16} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
